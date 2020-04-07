3bbbdf289aadb24296839f446758aca6c2269875
Dlray89 <dlrayjr89@gmail.com>
2020-04-06 23:08:34 -0400

    Update

--- personalwebsite/src/compTools/bottomNav.js
+++ personalwebsite/src/compTools/bottomNav.js
@@ -49,10 +49,10 @@ function NavBar(){
                site design &copy; 2020 dapthedev, inc
             </Typography>
             <CardActionArea className={classes.Icons}>
-            <Icon color="inherit" className="fab fa-linkedin"> <a href="https://www.linkedin.com/in/dapperdave1914/"></a> </Icon>
-            <Icon color="inherit" className="fab fa-github"><a href=""></a></Icon>
-            <Icon color="inherit" className="fab fa-facebook"><a href=""></a></Icon>
-            <Icon color="inherit" className="fab fa-medium"><a href="https://medium.com/@dlrayjr89"></a></Icon>
+            <Icon color="inherit" className="fab fa-linkedin"> <a href="https://www.linkedin.com/in/dapperdave1914/">LinkedIn</a> </Icon>
+            <Icon color="inherit" className="fab fa-github"><a href="https://medium.com/@dlrayjr89">GitHub</a></Icon>
+            <Icon color="inherit" className="fab fa-facebook"><a href="https://medium.com/@dlrayjr89">Facebook</a></Icon>
+            <Icon color="inherit" className="fab fa-medium"><a href="https://medium.com/@dlrayjr89">Medium</a></Icon>
             </CardActionArea>
         </Toolbar>
     </AppBar>
